88174cf772c760973d296f8ab0d6de282c6b5388
lraka <lufnaraka@gmail.com>
2024-04-22 01:02:21 +0700

    first commit

---
+++ fun-space/appy.py
@@ -0,0 +1,8 @@
+# Hitung Luas Segitiga sederhana
+def Luas_segitiga():
+    a = int(input("Masukkan alas segitiga: "))
+    t = int(input("Masukkan tinggi segitiga: "))
+    luas = a * t / 2
+    print("Luas segitiga adalah: ", luas)
+
+Luas_segitiga()
